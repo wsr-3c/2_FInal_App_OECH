eec9ab8e17e8dce191292d7488975fef81bb9901
WSR-3 <ITrt2UU3urp(>
2024-03-27 14:02:42 +0800

    version 0.2.2 icon - success splash download - success Splashscreen is was made - success

---
+++ app/src/main/java/ru/strannik/a2_final_app_oech/SplashScreen.java
@@ -0,0 +1,34 @@
+package ru.strannik.a2_final_app_oech;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash_screen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+            new Handler().post(new Runnable() {
+                @Override
+                public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+                }
+            });
+    }
+}
