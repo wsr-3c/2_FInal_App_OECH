047b33913989e7dbfb0550480f8d704f55e2d94c
WSR-3 <ITrt2UU3urp(>
2024-03-28 11:21:22 +0800

    version 5 icon - success splash download - success Splashscreen complete - success Onboarding1 complete - success Onboarding4 complete - success Onboarding3 complete - success
    Session 5
    panel 4buttons = success

--- app/src/main/java/ru/strannik/a2_final_app_oech/Track.java
+++ app/src/main/java/ru/strannik/a2_final_app_oech/Track.java
@@ -1,6 +1,10 @@
 package ru.strannik.a2_final_app_oech;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +14,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Track extends AppCompatActivity {
 
+    Button Button_home, Button_wallet, Button_profile;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +25,39 @@ public class Track extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        Button_home =findViewById(R.id.Button_home);
+
+        Button_home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Track.this, home.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button_wallet = findViewById(R.id.Button_wallet);
+
+        Button_wallet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Track.this, wallet.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button_profile = findViewById(R.id.Button_profile);
+
+        Button_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Track.this, Profile.class);
+                startActivity(intent);
+                finish();
+            }
         });
     }
 }
